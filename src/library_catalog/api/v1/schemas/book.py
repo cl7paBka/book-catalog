bec1bf64b2b3a14f478ad246359ff8681500576a
cl7paBka <maks-kandikov@yandex.ru>
2026-05-19 19:40:59 +0300

    feat: 3.3 book service for domain layer + book schemas in api layer

--- src/library_catalog/api/v1/schemas/book.py
+++ src/library_catalog/api/v1/schemas/book.py
@@ -17,3 +17,23 @@ class ShowBook(BaseModel):
     extra: dict | None
     created_at: datetime
     updated_at: datetime
+
+class BookCreate(ShowBook):
+    title: str
+    author: str
+    year: int
+    genre: str
+    pages: int
+    isbn: str | None
+    description: str
+
+class BookUpdate(BaseModel):
+    title: str | None = None
+    author: str | None = None
+    year: int | None = None
+    genre: str | None = None
+    pages: int | None = None
+    available: bool | None = None
+    isbn: str | None = None
+    description: str | None = None
+    extra: dict | None = None
